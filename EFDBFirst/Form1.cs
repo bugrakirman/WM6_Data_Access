49bc23031837ac1ec248e69a09da4274aaf6e042
Bugra Kırman <bugrakirman@gmail.com>
2018-12-25 13:48:43 +0300

    5 sorgu daha eklendi

--- EFDBFirst/Form1.cs
+++ EFDBFirst/Form1.cs
@@ -135,10 +135,11 @@ namespace EFDBFirst
                 (od, product) => new { od, product })
                 .GroupBy(x => x.product.ProductName)
                 .OrderBy(x => x.Key)
+                .ToList()//kaldırıp hatayı gör sql e convert i gönderemiyor
                 .Select(x => new
                 {
                     x.Key,
-                    Total = x.Sum(y => y.od.UnitPrice * y.od.Quantity)
+                    Total = x.Sum(y => y.od.UnitPrice * y.od.Quantity*Convert.ToDecimal((1-y.od.Discount)))
                 });
             //dgvTest.DataSource = sorgu9.ToList();
 
@@ -152,14 +153,119 @@ namespace EFDBFirst
                           by new
                           {
                               prod.ProductName
-                          } into gp
+                          } 
+                          into gp
                           orderby gp.Key.ProductName
                           select new
                           {
                               gp.Key.ProductName,
-                              Total = gp.Sum(x => x.od.UnitPrice * x.od.Quantity)
+
+                              //Total = gp.Sum(x => x.od.UnitPrice * x.od.Quantity*Convert.ToDecimal(1-x.od.Discount)) aynı hata
                           };
+
+            //var data = sorgu10.ToList();
+            //dgvTest.DataSource=data.GroupBy(x => x.ProductName)
+            //    .Select(x=> new {
+            //        ProductName=x.Key,
+            //        Total = gp.Sum(x => x.od.UnitPrice * x.od.Quantity * Convert.ToDecimal(1 - x.od.Discount))
+            //    }).tolist()   veriyi çektikten sonra işliyoruz hatayı düzeltmek için
+
            // dgvTest.DataSource = sorgu10.ToList();
+
+            //calisanlarim kac tane siparis almis 
+
+            var sorgu11 = from emp in db.Employees
+                          join o in db.Orders on emp.EmployeeID equals o.EmployeeID
+                          join od in db.Order_Details on o.OrderID equals od.OrderID
+                           group new
+                          {
+                              emp,
+                              od
+                          }
+                          by new
+                          {
+                              emp.FirstName,
+                              emp.LastName
+                          } 
+                          into gp
+                          select new
+                          {
+                              Employee = gp.Key.FirstName+" "+gp.Key.LastName,
+                              total = gp.Sum(x=>x.od.Quantity)
+                          };
+            //dgvTest.DataSource = sorgu11.OrderByDescending(x=>x.total).ToList();
+
+            var sorgu12 = db.Order_Details
+                .Join(db.Orders,
+                od => od.OrderID,
+                o => o.OrderID,
+                (od, o) => new { od, o })
+                .Join(db.Employees,
+                gg => gg.o.EmployeeID,
+                emp => emp.EmployeeID,
+                (gg, emp) => new { gg, emp })
+                .GroupBy(x => x.emp.FirstName + " " + x.emp.LastName)
+                .Select(x => new {
+                    employee = x.Key,
+                    total = x.Sum(y => y.gg.od.Quantity)
+                })
+                .OrderByDescending(x => x.total)
+                .ToList();
+            //dgvTest.DataSource = sorgu12;
+
+
+            //hangi kategoriden toplam kac adet siiparisim var
+
+            var sorgu13 = from c in db.Categories
+                          join p in db.Products on c.CategoryID equals p.CategoryID
+                          join od in db.Order_Details on p.ProductID equals od.ProductID
+                          group new
+                          {
+                              c,
+                              od
+                          }
+                         by new
+                         {
+                             c.CategoryName,
+                         }
+                          into gp
+                          select new
+                          {
+                              Kategori = gp.Key.CategoryName,
+                              total = gp.Sum(x => x.od.Quantity)
+                          };
+            //dgvTest.DataSource = sorgu13.OrderByDescending(x=>x.total).ToList();
+
+            //siparis no- toplam siparis tutari
+            var sorgu14=from od in db.Order_Details
+                        group new
+                        {
+                            od
+                        }
+                        by new
+                        {
+                            od.OrderID
+                        }
+                        into gp
+                        select new{
+                            gp.Key.OrderID,
+                            total = gp.Sum(x=>x.od.UnitPrice*x.od.Quantity)
+                        };
+            //dgvTest.DataSource = sorgu14.ToList();
+
+            var sorgu15 = db.Order_Details
+                .GroupBy(x => x.OrderID)
+                .ToList()
+                .Select(x => new
+                {
+                    x.Key,
+                    total = x.Sum(y => y.Quantity * y.UnitPrice * Convert.ToDecimal((1 - y.Discount)))
+                });
+            //dgvTest.DataSource = sorgu15;
+
+
+            //calisanlarim hangi kategoriden kac adet siparis vermistir 2 key olacak -- ödev
+
         }
     }
 }
