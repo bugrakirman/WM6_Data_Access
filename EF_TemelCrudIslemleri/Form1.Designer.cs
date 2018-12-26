0ae9f7b7d50d7806b45bd8810690afa641cc373f
Bugra Kırman <bugrakirman@gmail.com>
2018-12-26 11:52:54 +0300

    DBFirst ile update eklendi

--- EF_TemelCrudIslemleri/Form1.Designer.cs
+++ EF_TemelCrudIslemleri/Form1.Designer.cs
@@ -35,7 +35,14 @@
             this.btnKategoriKaydet = new System.Windows.Forms.Button();
             this.ep1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.lstUrunler = new System.Windows.Forms.ListBox();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.nudFiyat = new System.Windows.Forms.NumericUpDown();
+            this.cmbUrunKategori = new System.Windows.Forms.ComboBox();
+            this.btnUrunGuncelle = new System.Windows.Forms.Button();
+            this.gbUrun = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.ep1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).BeginInit();
+            this.gbUrun.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmbKategori
@@ -49,14 +56,14 @@
             // 
             // txtKategoriAdi
             // 
-            this.txtKategoriAdi.Location = new System.Drawing.Point(12, 39);
+            this.txtKategoriAdi.Location = new System.Drawing.Point(12, 50);
             this.txtKategoriAdi.Name = "txtKategoriAdi";
             this.txtKategoriAdi.Size = new System.Drawing.Size(121, 20);
             this.txtKategoriAdi.TabIndex = 1;
             // 
             // txtAciklama
             // 
-            this.txtAciklama.Location = new System.Drawing.Point(12, 65);
+            this.txtAciklama.Location = new System.Drawing.Point(12, 87);
             this.txtAciklama.Name = "txtAciklama";
             this.txtAciklama.Size = new System.Drawing.Size(121, 96);
             this.txtAciklama.TabIndex = 2;
@@ -64,11 +71,11 @@
             // 
             // btnKategoriKaydet
             // 
-            this.btnKategoriKaydet.Location = new System.Drawing.Point(12, 167);
+            this.btnKategoriKaydet.Location = new System.Drawing.Point(12, 200);
             this.btnKategoriKaydet.Name = "btnKategoriKaydet";
             this.btnKategoriKaydet.Size = new System.Drawing.Size(121, 21);
             this.btnKategoriKaydet.TabIndex = 3;
-            this.btnKategoriKaydet.Text = "button1";
+            this.btnKategoriKaydet.Text = "insert";
             this.btnKategoriKaydet.UseVisualStyleBackColor = true;
             this.btnKategoriKaydet.Click += new System.EventHandler(this.btnKategoriKaydet_Click);
             // 
@@ -79,17 +86,69 @@
             // lstUrunler
             // 
             this.lstUrunler.FormattingEnabled = true;
-            this.lstUrunler.Location = new System.Drawing.Point(139, 12);
+            this.lstUrunler.Location = new System.Drawing.Point(6, 19);
             this.lstUrunler.Name = "lstUrunler";
             this.lstUrunler.Size = new System.Drawing.Size(263, 173);
             this.lstUrunler.TabIndex = 4;
+            this.lstUrunler.SelectedIndexChanged += new System.EventHandler(this.lstUrunler_SelectedIndexChanged);
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(276, 19);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(127, 20);
+            this.txtUrunAdi.TabIndex = 5;
+            // 
+            // nudFiyat
+            // 
+            this.nudFiyat.Location = new System.Drawing.Point(276, 46);
+            this.nudFiyat.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudFiyat.Name = "nudFiyat";
+            this.nudFiyat.Size = new System.Drawing.Size(127, 20);
+            this.nudFiyat.TabIndex = 6;
+            // 
+            // cmbUrunKategori
+            // 
+            this.cmbUrunKategori.FormattingEnabled = true;
+            this.cmbUrunKategori.Location = new System.Drawing.Point(276, 73);
+            this.cmbUrunKategori.Name = "cmbUrunKategori";
+            this.cmbUrunKategori.Size = new System.Drawing.Size(127, 21);
+            this.cmbUrunKategori.TabIndex = 7;
+            // 
+            // btnUrunGuncelle
+            // 
+            this.btnUrunGuncelle.Location = new System.Drawing.Point(276, 101);
+            this.btnUrunGuncelle.Name = "btnUrunGuncelle";
+            this.btnUrunGuncelle.Size = new System.Drawing.Size(127, 23);
+            this.btnUrunGuncelle.TabIndex = 8;
+            this.btnUrunGuncelle.Text = "update";
+            this.btnUrunGuncelle.UseVisualStyleBackColor = true;
+            this.btnUrunGuncelle.Click += new System.EventHandler(this.btnUrunGuncelle_Click);
+            // 
+            // gbUrun
+            // 
+            this.gbUrun.Controls.Add(this.lstUrunler);
+            this.gbUrun.Controls.Add(this.btnUrunGuncelle);
+            this.gbUrun.Controls.Add(this.txtUrunAdi);
+            this.gbUrun.Controls.Add(this.cmbUrunKategori);
+            this.gbUrun.Controls.Add(this.nudFiyat);
+            this.gbUrun.Location = new System.Drawing.Point(139, 12);
+            this.gbUrun.Name = "gbUrun";
+            this.gbUrun.Size = new System.Drawing.Size(420, 209);
+            this.gbUrun.TabIndex = 9;
+            this.gbUrun.TabStop = false;
+            this.gbUrun.Text = "Urun";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lstUrunler);
+            this.Controls.Add(this.gbUrun);
             this.Controls.Add(this.btnKategoriKaydet);
             this.Controls.Add(this.txtAciklama);
             this.Controls.Add(this.txtKategoriAdi);
@@ -98,6 +157,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ep1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).EndInit();
+            this.gbUrun.ResumeLayout(false);
+            this.gbUrun.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +173,11 @@
         private System.Windows.Forms.Button btnKategoriKaydet;
         private System.Windows.Forms.ErrorProvider ep1;
         private System.Windows.Forms.ListBox lstUrunler;
+        private System.Windows.Forms.Button btnUrunGuncelle;
+        private System.Windows.Forms.ComboBox cmbUrunKategori;
+        private System.Windows.Forms.NumericUpDown nudFiyat;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.GroupBox gbUrun;
     }
 }
 
--- EF_TemelCrudIslemleri/Form1.cs
+++ EF_TemelCrudIslemleri/Form1.cs
@@ -27,7 +27,7 @@ namespace EF_TemelCrudIslemleri
         private void KategorileriGetir()
         {
             NorthwindsabahEntities db = new NorthwindsabahEntities();
-            cmbKategori.DataSource = db.Categories
+            var kategoriler1 = db.Categories
                 .OrderBy(x => x.CategoryName)
                 .Select(x => new CategoryViewModel() {
                     CategoryID = x.CategoryID,
@@ -35,6 +35,18 @@ namespace EF_TemelCrudIslemleri
                     ProductCount = x.Products.Count
                 })
                 .ToList();
+            var kategoriler2 = db.Categories
+                .OrderBy(x => x.CategoryName)
+                .Select(x => new CategoryViewModel()
+                {
+                    CategoryID = x.CategoryID,
+                    CategoryName = x.CategoryName,
+                    ProductCount = x.Products.Count
+                })
+                .ToList();
+            cmbUrunKategori.DataSource = kategoriler2;
+            cmbKategori.DataSource = kategoriler1;
+            
             //cmbKategori.DisplayMember = "CategoryName";
             //cmbKategori.ValueMember = "CategoryID";    tostringi ezince gerek kalmadı
         }
@@ -83,19 +95,78 @@ namespace EF_TemelCrudIslemleri
             //    .OrderBy(x => x.ProductName)
             //    .ToList(); ya da 
             //lstUrunler.DisplayMember = "ProductName";
-
-            lstUrunler.DataSource = db.Categories
+            var sorgu = db.Categories
                 .First(x => x.CategoryID == cat.CategoryID)
                 .Products
-                .Select(x=> new ProductViewModel() {
-                    ProductID=x.ProductID,
-                    ProductName=x.ProductName,
-                    UnitPrice=x.UnitPrice
+                .Select(x => new ProductViewModel()
+                {
+                    ProductID = x.ProductID,
+                    ProductName = x.ProductName,
+                    UnitPrice = x.UnitPrice
                 })
-                .OrderBy(x=>x.ProductName)
-                .ToList();        // first yerine Where(x => x.CategoryID == cat.CategoryID).First() yazılabilir.
+                .OrderBy(x => x.ProductName)
+                .ToList();   // first yerine Where(x => x.CategoryID == cat.CategoryID).First() yazılabilir.
+
+            lstUrunler.DataSource = sorgu;
+            gbUrun.Visible = sorgu.Count > 0;
 
             
+        }
+
+        private void lstUrunler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstUrunler.SelectedItem == null) return;
+
+            var seciliUrun = lstUrunler.SelectedItem as ProductViewModel;
+            NorthwindsabahEntities db = new NorthwindsabahEntities();
+            var urun = db.Products.Find(seciliUrun.ProductID); //sadece primary key üzerinden çalışır
+            txtUrunAdi.Text = urun.ProductName;
+            //nudFiyat.Value = urun.UnitPrice.HasValue?urun.UnitPrice.Value:0; uzun yöntem nullable decimali decimale çeviremedi
+            //nudFiyat.Value = urun.UnitPrice.GetValueOrDefault();  ya da 
+            nudFiyat.Value = urun.UnitPrice ?? 0;
+
+            var uruncatlist = cmbUrunKategori.DataSource as List<CategoryViewModel>;
+            foreach (var item in uruncatlist)
+            {
+                if (item.CategoryID==urun.CategoryID)
+                {
+                    cmbUrunKategori.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void btnUrunGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NorthwindsabahEntities db = new NorthwindsabahEntities();
+                var seciliUrun = lstUrunler.SelectedItem as ProductViewModel;
+                var urun = db.Products.Find(seciliUrun.ProductID); // referans tipindeki urunun propertylerini değiştirip savechange ile değişiklikleri sql e aktarıyor
+                urun.ProductName = txtUrunAdi.Text;
+                urun.UnitPrice = nudFiyat.Value;
+                urun.CategoryID = (cmbUrunKategori.SelectedItem as CategoryViewModel).CategoryID;
+                int sonuc = db.SaveChanges();
+                KategorileriGetir();
+                MessageBox.Show($"{sonuc} urun guncellendi");
+            }
+            catch(DbEntityValidationException ex)
+            {
+                foreach (var validationError in ex.EntityValidationErrors)
+                {
+                    foreach (var error in validationError.ValidationErrors)
+                    {
+                        if (error.PropertyName == "CategoryName")
+                            ep1.SetError(txtKategoriAdi, error.ErrorMessage);
+
+                        MessageBox.Show(EntityHelper.ValidationMessage(ex), "Bir hata olustu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
